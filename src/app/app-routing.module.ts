3217382c63c1af0e245bd02951ad18ba3f8256ac
OscarHendrix10 <Oscarhendrix74@gmail.com>
2021-09-26 19:40:35 -0500

    xd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { EditarComponent } from './pages/editar/editar.component';
 import { MostrarComponent } from './pages/mostrar/mostrar.component';
 import { ModificarComponent } from './pages/modificar/modificar.component';
 
 //IMPLEMANTACION DE RUTAS ESTABLECIDAS ENE LE SISTEMA CON SU DEFAULT
 const routes: Routes = [{
-  path: "registro", component: RegistroComponent 
-},{
-  path: "editar", component: EditarComponent
-},{path: "mostrar", component: MostrarComponent
-},{
-  path: "modificar/:id", component: ModificarComponent
-},
-{
-  path: "**", component: RegistroComponent
-}];
+  path: "registro", component: RegistroComponent },
+  {path: "mostrar", component: MostrarComponent},
+  {path: "modificar/:id", component: ModificarComponent},
+  {path: "**", component: RegistroComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
